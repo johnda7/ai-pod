37ad18924fb42fc547a5ae60e86fc25dbbb8aaf2
johnda7 <anotherstoriz@gmail.com>
2025-12-04 17:17:20 +0000

    fix: 7 исправлений UI - видео, Life Skills, рефлексия, помодоро, планировщик, цели

--- assets/NotesTool-8spAf6AN.js
+++ assets/NotesTool-8spAf6AN.js
@@ -1,4 +1,4 @@
-import{c as P,r as i,j as e,A as T,m as k,X as C,o as q,S as K,f as Q,T as V,k as I}from"./index-DI5uzzHj.js";import{u as U}from"./useSyncTool-C8ERaAi5.js";/**
+import{c as P,r as i,j as e,A as T,m as k,X as C,o as q,S as K,f as Q,T as V,k as I}from"./index-C_OYZS1b.js";import{u as U}from"./useSyncTool-rspsrgLk.js";/**
  * @license lucide-react v0.554.0 - ISC
  *
  * This source code is licensed under the ISC license.
